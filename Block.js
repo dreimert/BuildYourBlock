eb8d58d947787277f76c56dc166d165b8c070579
Nick Schrader <nick.schrader@mailbox.org>
2019-02-27 10:49:26 +0000

    First Stage

--- Block.js
+++ Block.js
@@ -1,9 +1,19 @@
-function generateId() {
-  return Math.floor(Math.random()*1000000000);
+const crypto = require('crypto');
+
+function getHash(data) {
+  return crypto.createHash('sha256').update(data, 'utf8').digest('hex');
+}
+
+function generateId(date, previous, data) {
+  return getHash(date + previous + data)
 }
 
 module.exports = class Block {
   // Complétez le constructeur
   constructor(previous, data) {
+    this.previous = previous;
+    this.data = data;
+    this.date = new Date();
+    this.id = generateId(this.date, previous, data);
   }
 }
