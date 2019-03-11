b9ccb43b53d74e1ffe74ebb791c92aaa7fe377d1
Damien Reimert <damien.reimert@gmail.com>
2019-03-11 15:54:23 +0100

    Correction etape 1

--- Block-1.js
+++ Block-1.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const { BlockTool } = require('./tools');
 
 // Vous n'avez pas à comprendre BlockTool.
@@ -7,19 +9,23 @@ const { BlockTool } = require('./tools');
 module.exports = class Block extends BlockTool {
 
   // Complétez le constructeur
-  constructor(index, data) {
+  constructor(index, previous, data) {
     super() // Obligatoire car on hérite de BlockTool
     // Le mot clé `this` permet d'accèder aux propriétés de l'object depuis ses méthodes.
     this.index = index;
-    //...
+    this.previous = previous;
+    this.data = data;
+    this.id = this.getHash();
   }
 
   // Retourne l'identifiant du block en le calculant depuis les données
-  getHash() {}
+  getHash() {
+    return crypto.createHash('sha256').update(`${this.index}${this.previous}${this.data}`, 'utf8').digest('hex');
+  }
 
   // Retourne un boolean qui indique si le block est valide
   isValid() {
-    return true;
+    return this.getHash() === this.id;
   }
 
   // Utile à l'étape 2
